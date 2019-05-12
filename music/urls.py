578c96426854f1f891081dd4d209494acff5206a
Prateek Sachan <psachan@cs.iitr.ac.in>
2019-05-12 11:34:38 +0530

    Added forms

--- music/urls.py
+++ music/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name='music'
+app_name = 'music'
 
 urlpatterns = [
-    path('',views.index,name='home'),
-    path('<int:album_id>/',views.details,name='details')
+    path('', views.index, name='home'),
+    path('<int:album_id>/', views.details, name='details'),
+    path('<int:album_id>/favourite/', views.favourite, name='favourite')
 ]
--- music/views.py
+++ music/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .models import Album, Song
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -23,3 +23,15 @@ def details(request, album_id):
     except Album.DoesNotExist:
         raise Http404('Album Does Not Exists')
     return render(request, 'music/details.html', {'songs': album.song_set.all(), 'album': album})
+
+
+def favourite(request, album_id):
+    album = get_object_or_404(Album, id=album_id)
+    try:
+        selected_song = album.song_set.get(id=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/details.html', {'songs': album.song_set.all(), 'album': album, 'error_message': "You have not selected any item"})
+    else:
+        selected_song.is_favourite = True
+        selected_song.save()
+        return render(request, 'music/details.html', {'songs': album.song_set.all(), 'album': album})
